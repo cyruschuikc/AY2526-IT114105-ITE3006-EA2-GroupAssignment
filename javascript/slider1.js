0876c31c5a7be8513a40671c7758953f0e825b9c
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-04-19 15:25:10 +0800

    Add files via upload

---
+++ javascript/slider1.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector('.slider-images');
+const imgs = document.querySelectorAll('.slider-images img');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let index = 0;
+let interval;
+
+function showSlide(n) {
+  index = (n + imgs.length) % imgs.length;
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+  showSlide(index - 1);
+});
+
+nextButton.addEventListener('click', () => {
+  showSlide(index + 1);
+});
+
+// auto-slide every 3 seconds
+function startRotation() {
+  interval = setInterval(() => {
+    showSlide(index + 1);
+  }, 3000);
+}
+function stopRotation() {
+  clearInterval(interval);
+}
+
+// pause on hover
+document.querySelector('.slider').addEventListener('mouseenter', stopRotation);
+document.querySelector('.slider').addEventListener('mouseleave', startRotation);
+
+startRotation();
